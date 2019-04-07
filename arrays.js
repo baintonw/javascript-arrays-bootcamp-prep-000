7e33d3d5571374231be5997ac66fd0bebad46a5a
William Bainton <baintonw@gmail.com>
2019-04-07 21:35:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,15 +11,24 @@ var  chocolateBars = [
   
 function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
+<<<<<<< HEAD
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
+=======
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
+>>>>>>> 93801d202f259c9b527734a03a0337d7bbd66add
   return array
 }
 
 function addElementToEndOfArray(array, element) {
   return [...array, element]
+<<<<<<< HEAD
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
@@ -50,6 +59,13 @@ function destructivelyRemoveElementFromEndOfArray (array) {
 function removeElementFromEndOfArray (array) {
   array.slice(0, array.length - 1)
   array = array.slice(0, array.length - 1)
+=======
+  return  array
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element)
+>>>>>>> 93801d202f259c9b527734a03a0337d7bbd66add
   return array
 }
 
